0ae3ff28311c93523889c1f644077f4d804d9da2
Michael-Charara <chararam@outlook.com>
2021-01-14 13:13:10 -0500

    Lab 3 completed
    also created backup for lab 3

---
+++ CreateWithCode/LayinSpace - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playeRb;
+
+    private float speed = 10.0f;
+    private float zBound = 6.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playeRb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+
+        ConstrainPlayerPosition();
+    }
+
+    // Moves player based on arrow key input
+
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playeRb.AddForce(Vector3.forward * speed * verticalInput);
+        playeRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    // Prevent from leaving the top or bottom of screen
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+
+    }
+}
