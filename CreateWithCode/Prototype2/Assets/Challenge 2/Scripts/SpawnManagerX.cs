bed8e85ca8fed3dd25afaf739c52837e0997d68d
LayinSpace <layinspacestudio@gmail.com>
2021-01-13 19:19:06 -0500

    complete challenge 2 Play Fetch

---
+++ CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    public float timer;
+
+    private float minTime = 3.0f;
+    private float maxTime = 5.0f;
+
+    private float spawnTimeInterval;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimeInterval = Random.Range(minTime, maxTime);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > spawnTimeInterval)
+        {
+            // call function
+            SpawnRandomBall();
+            // reset timer and spawn interval
+            spawnTimeInterval = Random.Range(minTime, maxTime);
+            timer = 0;
+
+        }
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+}
